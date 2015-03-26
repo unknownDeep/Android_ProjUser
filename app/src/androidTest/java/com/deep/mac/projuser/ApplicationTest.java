ae5d07f7f060309ba2cd7aabdc604dfe8fba5cdd
UnknownDeep <unknowndeep@163.com>
2015-03-26 22:42:39 +0800

    create proj

---
+++ app/src/androidTest/java/com/deep/mac/projuser/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.deep.mac.projuser;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
